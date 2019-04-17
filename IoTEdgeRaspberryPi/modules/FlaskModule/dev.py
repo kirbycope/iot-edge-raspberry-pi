a0593d511f87dab0d19c0b564c9ef91577bbc9bd
Timothy Cope <kirbycope@gmail.com>
2019-04-17 14:59:13 -0600

    More styles

--- IoTEdgeRaspberryPi/modules/FlaskModule/dev.py
+++ IoTEdgeRaspberryPi/modules/FlaskModule/dev.py
@@ -6,8 +6,6 @@ import socket
 
 
 def start():
-    getDirectoryName
-    getHostIp()
     app = Flask(__name__)
     app.register_blueprint(routes)
     app.run(host="0.0.0.0", port=1337, debug=True)
